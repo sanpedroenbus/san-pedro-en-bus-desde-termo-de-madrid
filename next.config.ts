f5bba0b8298e8197f609d3a96e15d1b648bbc640
sanpedroenbus <sanpedroenbus@gmail.com>
2026-08-17 17:59:35 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: false,
+  cacheComponents: true,
   allowedDevOrigins: ["127.0.0.1"],
   devIndicators: false,
   async headers() {
